3e0be5aa7432777ee65ab8b5fc3765a159f74532
BhagatBhutale <bhagatbhutale57@gmail.com>
2025-02-19 22:37:22 +0530

    body color change using js minitask 1

---
+++ 1-bodycolor/app.js
@@ -0,0 +1,14 @@
+
+let bodyEle = document.querySelector("body")
+
+function handlered() {
+bodyEle.style.backgroundColor = "red"
+}
+
+function handleyellow() {
+    bodyEle.style.backgroundColor = "yellow"
+}
+
+function handlegray() {
+    bodyEle.style.backgroundColor = "gray"
+}
